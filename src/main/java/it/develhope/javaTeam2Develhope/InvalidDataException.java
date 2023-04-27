a0d936647cd4e5e7b0c13d59e9e5066618aa15c7
Giulia <giuliac35@gmail.com>
2023-04-27 14:28:52 +0200

    Delete exception class, add yml file to gitignore

--- src/main/java/it/develhope/javaTeam2Develhope/InvalidDataException.java
+++
@@ -1,7 +0,0 @@
-package it.develhope.javaTeam2Develhope;
-
-public class InvalidDataException extends RuntimeException {
-    public InvalidDataException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
