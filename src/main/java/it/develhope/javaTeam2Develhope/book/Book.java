c1dafb808f60004b892840ee7d68e391ce105cea
Giulia <giuliac35@gmail.com>
2023-05-04 12:23:01 +0200

    Changed credentials, removed Spring security, add Spring validation dependency

--- src/main/java/it/develhope/javaTeam2Develhope/book/Book.java
+++ src/main/java/it/develhope/javaTeam2Develhope/book/Book.java
@@ -1,12 +1,11 @@
 package it.develhope.javaTeam2Develhope.book;
-
-
-import it.develhope.javaTeam2Develhope.game.Game;
-import it.develhope.javaTeam2Develhope.motionPicture.MotionPicture;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.lang.NonNull;
 
 
 import java.util.List;
@@ -21,16 +20,26 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @NotBlank (message = "mandatory")
     private String author;
+    @NotBlank (message = "mandatory")
     private String title;
+    @NotBlank (message = "mandatory")
     private String topic;
+    @NotBlank (message = "mandatory")
     private String publisher;
+    @NotBlank (message = "mandatory")
     @Column(unique = true)
     private String ISBN;
-    private int year;
-    private float price;
-    private int numberOfPages;
-    private boolean isInStock;
+    @NotNull
+    private Integer year;
+    @NotNull
+    private Float price;
+    @NotNull
+    private Integer numberOfPages;
+    @NotNull
+    private Boolean isInStock;
     private String imageCoverFilePath;
-    private boolean isEbook;
+    @NotNull
+    private Boolean isEbook;
 }
