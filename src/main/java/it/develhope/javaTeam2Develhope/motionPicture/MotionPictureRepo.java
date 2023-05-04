61739d0b373de94e64c1c1bc3bb3159fc1a37a3b
Giulia <giuliac35@gmail.com>
2023-05-04 16:31:49 +0200

    Fix method in MotionPicturesRepo

--- src/main/java/it/develhope/javaTeam2Develhope/motionPicture/MotionPictureRepo.java
+++ src/main/java/it/develhope/javaTeam2Develhope/motionPicture/MotionPictureRepo.java
@@ -1,9 +1,14 @@
 package it.develhope.javaTeam2Develhope.motionPicture;
 
+import it.develhope.javaTeam2Develhope.game.Game;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MotionPictureRepo extends JpaRepository<MotionPicture,Long> {
+    Page<MotionPicture> findAll(Specification<MotionPicture> spec, Pageable paging);
 
 }
