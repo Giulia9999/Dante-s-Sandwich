6a36cba56d0aae473efa9c0e832d4a908bdf670b
Giulia <giuliac35@gmail.com>
2023-05-04 16:30:03 +0200

    Add crud method to GameController and MotionPicturesController

--- src/main/java/it/develhope/javaTeam2Develhope/game/GameRepo.java
+++ src/main/java/it/develhope/javaTeam2Develhope/game/GameRepo.java
@@ -1,10 +1,14 @@
 package it.develhope.javaTeam2Develhope.game;
 
+import it.develhope.javaTeam2Develhope.book.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GameRepo extends JpaRepository<Game,Long> {
-
+    Page<Game> findAll(Specification<Game> spec, Pageable paging);
 
 }
