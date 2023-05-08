3884716b10278830fef796fd01642f1069b788e8
Alessio Limina <alessio.limina@gmail.com>
2023-05-08 21:42:27 +0200

    CRUD methods added on OrderController

--- src/main/java/it/develhope/javaTeam2Develhope/order/OrderRepo.java
+++ src/main/java/it/develhope/javaTeam2Develhope/order/OrderRepo.java
@@ -1,5 +1,9 @@
 package it.develhope.javaTeam2Develhope.order;
 
+import it.develhope.javaTeam2Develhope.digitalPurchase.DigitalPurchase;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +11,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OrderRepo extends JpaRepository<Order,Long> {
 
+    Page<Order> findAll(Specification<DigitalPurchase> spec, Pageable paging);
 }
 
