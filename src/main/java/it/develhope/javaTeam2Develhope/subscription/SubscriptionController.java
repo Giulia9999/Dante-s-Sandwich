aa324ce325f7b40c386e8d63d39d790c4a070f53
Giulia <giuliac35@gmail.com>
2023-05-11 15:13:24 +0200

    Put logic of all controller classes in the service classes

--- src/main/java/it/develhope/javaTeam2Develhope/subscription/SubscriptionController.java
+++ src/main/java/it/develhope/javaTeam2Develhope/subscription/SubscriptionController.java
@@ -23,128 +23,82 @@ import java.util.Set;
 @RequestMapping("/subscriptions")
 public class SubscriptionController {
     @Autowired
-    private SubscriptionRepo subscriptionRepo;
+    private SubscriptionService subscriptionService;
 
     @GetMapping
     public ResponseEntity<Page<Subscription>> getAllSubscriptions(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size,
             @RequestParam(required = false) Boolean isApproved,
             @RequestParam(required = false) Boolean isCanceled,
             @RequestParam(required = false) Boolean isRenewed,
-            @RequestParam(required = false) Float monthlyPrice) {
+            @RequestParam(required = false) Float monthlyPrice,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
 
-        Specification<Subscription> spec = Specification.where(null);
+        Page<Subscription> subscriptionsPage = subscriptionService.getAllSubscriptions(isApproved, isCanceled, isRenewed, monthlyPrice, page, size);
 
-        if (isApproved != null) {
-            spec = spec.and((root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("isApproved"), isApproved));
-        }
-        if (isCanceled != null) {
-            spec = spec.and((root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("isCanceled"), isCanceled));
-        }
-        if (isRenewed != null) {
-            spec = spec.and((root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("isRenewed"), isRenewed));
-        }
-        if (monthlyPrice != null) {
-            spec = spec.and((root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("monthlyPrice"), monthlyPrice));
-        }
-
-        Pageable paging = PageRequest.of(page, size);
-        Page<Subscription> subscriptionsPage = subscriptionRepo.findAll(spec, paging);
         return ResponseEntity.ok(subscriptionsPage);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Subscription> getSubscriptionById(@PathVariable long id) {
-        Optional<Subscription> optionalSubscription = subscriptionRepo.findById(id);
+        Optional<Subscription> optionalSubscription = subscriptionService.getSubscriptionById(id);
 
-        if (optionalSubscription.isEmpty()) {
+        if (optionalSubscription.isPresent()) {
+            Subscription subscription = optionalSubscription.get();
+            return ResponseEntity.ok(subscription);
+        } else {
             return ResponseEntity.notFound().build();
         }
-
-        Subscription subscription = optionalSubscription.get();
-
-        return ResponseEntity.ok(subscription);
     }
 
     @PostMapping("/multiple")
     public ResponseEntity<List<Subscription>> addMultipleSubscriptions(@RequestBody List<Subscription> subscriptions) {
-        List<Subscription> savedSubscriptions = subscriptionRepo.saveAllAndFlush(subscriptions);
-        return ResponseEntity.status(HttpStatus.CREATED).body(savedSubscriptions);
+        List<Subscription> savedSubscriptions = subscriptionService.addMultipleSubscriptions(subscriptions);
+
+        return ResponseEntity.ok(savedSubscriptions);
     }
 
-    @PostMapping("/single")
+    @PostMapping
     public ResponseEntity<Subscription> addSingleSubscription(@RequestBody Subscription subscription) {
-        Subscription savedSubscription = subscriptionRepo.saveAndFlush(subscription);
-        return ResponseEntity.status(HttpStatus.CREATED).body(savedSubscription);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Subscription> updateSubscription(@PathVariable Long id, @Valid @RequestBody Subscription subscription) {
-        Optional<Subscription> optionalSubscription = subscriptionRepo.findById(id);
-
-        if (optionalSubscription.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-
-        subscription.setId(id);
-
-        Subscription updatedSubscription = subscriptionRepo.save(subscription);
-        return ResponseEntity.ok(updatedSubscription);
-    }
-
-    @PatchMapping("/{id}")
-    public ResponseEntity<Subscription> updateSubscription(@PathVariable long id, @RequestBody Subscription subscription) {
-        Optional<Subscription> optionalSubscription = subscriptionRepo.findById(id);
-
-        if (optionalSubscription.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-
-        Subscription existingSubscription = optionalSubscription.get();
-
-        BeanUtils.copyProperties(subscription, existingSubscription, getEmptyPropertyNames(subscription));
-        Subscription savedSubscription = subscriptionRepo.save(existingSubscription);
+        Subscription savedSubscription = subscriptionService.addSingleSubscription(subscription);
 
         return ResponseEntity.ok(savedSubscription);
     }
 
-    private String[] getEmptyPropertyNames(Subscription source) {
-        final BeanWrapper src = new BeanWrapperImpl(source);
-        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+    @PutMapping("/{id}")
+    public ResponseEntity<Subscription> updateSubscription(@PathVariable Long id, @RequestBody Subscription subscription) {
+        Optional<Subscription> optionalSubscription = subscriptionService.updateSubscription(id, subscription);
 
-        Set<String> emptyNames = new HashSet<>();
-        for (java.beans.PropertyDescriptor pd : pds) {
-            Object srcValue = src.getPropertyValue(pd.getName());
-            if (srcValue instanceof String && StringUtils.isBlank((String) srcValue)) {
-                // Ignore empty string values
-                continue;
-            }
-            if (srcValue == null) {
-                // Include null values
-                emptyNames.add(pd.getName());
-            }
+        if (optionalSubscription.isPresent()) {
+            Subscription updatedSubscription = optionalSubscription.get();
+            return ResponseEntity.ok(updatedSubscription);
+        } else {
+            return ResponseEntity.notFound().build();
         }
-        String[] result = new String[emptyNames.size()];
-        return emptyNames.toArray(result);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Subscription> updateSubscriptionPartially(@PathVariable long id, @RequestBody Subscription subscription) {
+        Optional<Subscription> optionalSubscription = subscriptionService.updateSubscriptionPartially(id, subscription);
+
+        if (optionalSubscription.isPresent()) {
+            Subscription updatedSubscription = optionalSubscription.get();
+            return ResponseEntity.ok(updatedSubscription);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteSubscription(@PathVariable long id) {
-        Optional<Subscription> optionalSubscription = subscriptionRepo.findById(id);
-
-        if (optionalSubscription.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-
-        subscriptionRepo.deleteById(id);
+        subscriptionService.deleteSubscription(id);
 
         return ResponseEntity.noContent().build();
     }
 
-    @DeleteMapping("/clear")
+    @DeleteMapping
     public ResponseEntity<Void> deleteAllSubscriptions() {
-        subscriptionRepo.deleteAll();
+        subscriptionService.deleteAllSubscriptions();
 
         return ResponseEntity.noContent().build();
     }
--- src/main/java/it/develhope/javaTeam2Develhope/subscription/SubscriptionService.java
+++ src/main/java/it/develhope/javaTeam2Develhope/subscription/SubscriptionService.java
@@ -1,8 +1,118 @@
 package it.develhope.javaTeam2Develhope.subscription;
 
 import org.springframework.stereotype.Service;
+import io.micrometer.common.util.StringUtils;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 @Service
-
 public class SubscriptionService {
+    @Autowired
+    private SubscriptionRepo subscriptionRepo;
+
+    public Page<Subscription> getAllSubscriptions(Boolean isApproved, Boolean isCanceled, Boolean isRenewed, Float monthlyPrice, int page, int size) {
+        Specification<Subscription> spec = Specification.where(null);
+
+        if (isApproved != null) {
+            spec = spec.and((root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("isApproved"), isApproved));
+        }
+        if (isCanceled != null) {
+            spec = spec.and((root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("isCanceled"), isCanceled));
+        }
+        if (isRenewed != null) {
+            spec = spec.and((root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("isRenewed"), isRenewed));
+        }
+        if (monthlyPrice != null) {
+            spec = spec.and((root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("monthlyPrice"), monthlyPrice));
+        }
+
+        Pageable paging = PageRequest.of(page, size);
+        Page<Subscription> subscriptionsPage = subscriptionRepo.findAll(spec, paging);
+        return subscriptionsPage;
+    }
+
+    public Optional<Subscription> getSubscriptionById(long id) {
+        Optional<Subscription> optionalSubscription = subscriptionRepo.findById(id);
+        return optionalSubscription;
+    }
+
+    public List<Subscription> addMultipleSubscriptions(List<Subscription> subscriptions) {
+        List<Subscription> savedSubscriptions = subscriptionRepo.saveAllAndFlush(subscriptions);
+        return savedSubscriptions;
+    }
+
+    public Subscription addSingleSubscription(Subscription subscription) {
+        Subscription savedSubscription = subscriptionRepo.saveAndFlush(subscription);
+        return savedSubscription;
+    }
+
+    public Optional<Subscription> updateSubscription(Long id, Subscription subscription) {
+        Optional<Subscription> optionalSubscription = subscriptionRepo.findById(id);
+
+        if (optionalSubscription.isEmpty()) {
+            return Optional.empty();
+        }
+
+        subscription.setId(id);
+
+        Subscription updatedSubscription = subscriptionRepo.save(subscription);
+        return Optional.of(updatedSubscription);
+    }
+
+    public Optional<Subscription> updateSubscriptionPartially(long id, Subscription subscription) {
+        Optional<Subscription> optionalSubscription = subscriptionRepo.findById(id);
+
+        if (optionalSubscription.isEmpty()) {
+            return Optional.empty();
+        }
+
+        Subscription existingSubscription = optionalSubscription.get();
+
+        BeanUtils.copyProperties(subscription, existingSubscription, getEmptyPropertyNames(subscription));
+        Subscription savedSubscription = subscriptionRepo.save(existingSubscription);
+
+        return Optional.of(savedSubscription);
+    }
+
+    private String[] getEmptyPropertyNames(Subscription source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+
+        Set<String> emptyNames = new HashSet<>();
+        for (java.beans.PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue instanceof String && StringUtils.isBlank((String) srcValue)) {
+                // Ignore empty string values
+                continue;
+            }
+            if (srcValue == null) {
+                // Include null values
+                emptyNames.add(pd.getName());
+            }
+        }
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
+    }
+
+    public void deleteSubscription(long id) {
+        subscriptionRepo.deleteById(id);
+    }
+
+    public void deleteAllSubscriptions() {
+        subscriptionRepo.deleteAll();
+    }
 }
