6e3cb55df3c41b1a255897665f8f53e4165c0a01
Giulia <giuliac35@gmail.com>
2023-05-11 18:43:20 +0200

    Removed errors

--- src/main/java/it/develhope/javaTeam2Develhope/subscription/SubscriptionController.java
+++ src/main/java/it/develhope/javaTeam2Develhope/subscription/SubscriptionController.java
@@ -1,16 +1,7 @@
 package it.develhope.javaTeam2Develhope.subscription;
-
-import io.micrometer.common.util.StringUtils;
-import jakarta.validation.Valid;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.BeanWrapper;
-import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
